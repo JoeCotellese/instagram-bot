8f6d9101fbdb24146bcfa4229694cdfb5a6734a2
Joe Cotellese <joe@cotellese.net>
2018-12-31 16:56:30 -0500

    refactor script into function

--- follow-unfollow.py
+++ follow-unfollow.py
@@ -4,49 +4,52 @@ from instapy import InstaPy, Settings
 from instapy.util import smart_run
 
 
-
-config = yaml.load(open('./config.yml', 'r'))
-# get an InstaPy session!
-# set headless_browser=True to run InstaPy in the background
-Settings.chromedriver_location = config['settings']['chromedriver_location']
-Settings.log_location = config['settings']['log_location']
-Settings.database_location = config['settings']['db_location']
-session = InstaPy(username=config['auth']['username'],
-                  password=config['auth']['password'],
-                  headless_browser=False)
-
-
-with smart_run(session):
-    """ Activity flow """
-    # general settings
-    session.set_relationship_bounds(enabled=True,
-                                     delimit_by_numbers=True,
-                                      max_followers=4590,
-                                      min_followers=45,
-                                      min_following=77)
-
-    #session.set_dont_include(["friend1", "friend2", "friend3"])
-    #session.set_dont_like(["pizza", "#store"])
+def run_bot(config):    
+    # get an InstaPy session!
+    # set headless_browser=True to run InstaPy in the background
+    Settings.chromedriver_location = config['settings']['chromedriver_location']
+    Settings.log_location = config['settings']['log_location']
+    Settings.database_location = config['settings']['db_location']
+    session = InstaPy(username=config['auth']['username'],
+                    password=config['auth']['password'],
+                    headless_browser=False)
 
 
-    # activities
+    with smart_run(session):
+        """ Activity flow """
+        # general settings
+        session.set_relationship_bounds(enabled=True,
+                                        delimit_by_numbers=True,
+                                        max_followers=4590,
+                                        min_followers=45,
+                                        min_following=77)
 
-    """ Massive Follow of users followers (I suggest to follow not less than 3500/4000 users for better results)...
-    """
-    session.follow_user_followers(['stalkfollowlove','lindos.rhodes.greece','suloinenjoutilaisuus'], amount=800, randomize=False, interact=False)
+        #session.set_dont_include(["friend1", "friend2", "friend3"])
+        #session.set_dont_like(["pizza", "#store"])
 
-    """ First step of Unfollow action - Unfollow not follower users...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
 
-    """ Second step of Massive Follow...
-    """
-    session.follow_user_followers(['reflectiongram','elponzophoto','briannamadia'], amount=800, randomize=False, interact=False)
+        # activities
 
-    """ Second step of Unfollow action - Unfollow not follower users...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+        """ Massive Follow of users followers (I suggest to follow not less than 3500/4000 users for better results)...
+        """
+        session.follow_user_followers(['stalkfollowlove','lindos.rhodes.greece','suloinenjoutilaisuus'], amount=800, randomize=False, interact=False)
 
-    """ Clean all followed user - Unfollow all users followed by InstaPy...
-    """
-    session.unfollow_users(amount=500, InstapyFollowed=(True, "all"), style="FIFO", unfollow_after=24*60*60, sleep_delay=601)
+        """ First step of Unfollow action - Unfollow not follower users...
+        """
+        session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+
+        """ Second step of Massive Follow...
+        """
+        session.follow_user_followers(['reflectiongram','elponzophoto','briannamadia'], amount=800, randomize=False, interact=False)
+
+        """ Second step of Unfollow action - Unfollow not follower users...
+        """
+        session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+
+        """ Clean all followed user - Unfollow all users followed by InstaPy...
+        """
+        session.unfollow_users(amount=500, InstapyFollowed=(True, "all"), style="FIFO", unfollow_after=24*60*60, sleep_delay=601)
+
+if __name__ == "__main__":
+    config = yaml.load(open('./config.yml', 'r'))
+    run_bot(config)
