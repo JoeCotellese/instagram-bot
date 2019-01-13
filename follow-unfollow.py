9022c26b83b6954feb6ce7b2c5438b4f8c137814
Joe Cotellese <joe@cotellese.net>
2019-01-13 07:37:19 -0500

    Set delay from config file

--- follow-unfollow.py
+++ follow-unfollow.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+from random import shuffle
 from instapy import InstaPy, Settings
 from instapy.util import smart_run
 
@@ -12,9 +13,11 @@ def run_bot(config):
     Settings.database_location = config['settings']['db_location']
     session = InstaPy(username=config['auth']['username'],
                     password=config['auth']['password'],
-                    headless_browser=False)
+                    headless_browser=True,
+                    multi_logs=True)
 
-
+    follow_delay = config['settings']['follow_delay']
+    unfollow_delay = config['settings']['unfollow_delay']
     with smart_run(session):
         """ Activity flow """
         # general settings
@@ -26,19 +29,20 @@ def run_bot(config):
 
         # activities
         follow_list = config['follow_followers']
+        shuffle(follow_list)
         for follow in follow_list:
             """ Massive Follow of users followers (I suggest to follow not less than 3500/4000 users for better results)...
             """
-            session.follow_user_followers(follow, amount=100, randomize=True, interact=False)
+            session.follow_user_followers(follow, amount=800, randomize=True, interact=False, sleep_delay=follow_delay)
 
             """ First step of Unfollow action - Unfollow not follower users...
             """
-            session.unfollow_users(amount=50, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=601)
+            session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"), style="FIFO", unfollow_after=12*60*60, sleep_delay=unfollow_delay)
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
         print ("Missing config file name")
         sys.exit()
-    config = yaml.load(open(filename, 'r'))
+    config = yaml.load(open(sys.argv[1], 'r'))
     run_bot(config)
 
